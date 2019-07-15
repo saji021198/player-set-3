12e725aa121d976a36220234afa0b6653690ee4a
saji021198 <52283311+saji021198@users.noreply.github.com>
2019-07-14 20:02:59 -0700

    Update point.py

--- point.py
+++ point.py
@@ -1,7 +1,7 @@
 z,t=map(int,input().split(" "))
 r,s=map(int,input().split(" "))
 a,b=map(int,input().split(" "))
-if((z==r==a) or (t==s==b)):
+if((z==r==a) or (z==t) or (r==s) or (a==b)):
      print("yes")
 else:
      print("no")

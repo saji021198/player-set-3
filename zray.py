c4cb7d4f413089eecdea4a8dba80edfe5f931c35
saji021198 <52283311+saji021198@users.noreply.github.com>
2019-07-18 10:10:26 -0700

    Update zray.py

--- zray.py
+++ zray.py
@@ -1,10 +1,13 @@
 a,b=map(int,input().split())
 o=list(map(int,input().split()))
+v=0
 for i in range(0,len(o)):
     for j in range(1,len(o)):
         sum=o[i]+o[j]
         if sum==b:
-            print("yes")
-    break        
+            v=1
+if v==1:
+    print("yes")
 else:
     print("no")
+
